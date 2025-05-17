a27ab0584caae197d91588e98e8fbd59302a4fce
rajmendra <rajcool.lko@gmail.com>
2025-05-18 00:07:01 +0530

    test: return 0 if empty string is passed

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -4,4 +4,7 @@ describe('String Calculator', () => {
   test('add method is defined', () => {
     expect(typeof add).toBe('function');
   });
+  test('returns 0 for empty string', () => {
+    expect(add('')).toBe(0);
+  });
 });
